5df04c905c5549e1247aced6401afb196ac8ca68
SeopHO <56343122+SeopHO@users.noreply.github.com>
2021-01-27 00:00:09 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+const SongInformation=[
+    {
+        title: "Here with me",
+        writer: "marshmello",
+        img:"banner-bg.jpg",
+    },
+
+    {
+        title: "Happier",
+        writer: "marshmello",
+        img:"banner-bg.jpg",
+    },
+
+    {
+        title: "alone",
+        writer: "marshmello",
+        img:"banner-bg.jpg",
+    },
+
+    {
+        title: "Shelter",
+        writer: "Porter Robinson",
+        img:"banner-bg.jpg",
+    },
+
+    {
+        title: "legend never die",
+        writer: "legue of legends",
+        img:"banner-bg.jpg",
+    },
+
+    {
+        title: "Rise",
+        writer: "legue of legends",
+        img:"banner-bg.jpg",
+    },
+]
+
+console.log(screen.width/2);
+
