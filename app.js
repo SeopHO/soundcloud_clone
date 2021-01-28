f4385127885160e40bdfcc9df5c42edde7d4ae87
SeopHO <56343122+SeopHO@users.noreply.github.com>
2021-01-28 20:45:59 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -34,23 +34,58 @@ const SongInformation=[
         writer: "legue of legends",
         img:"banner-bg.jpg",
     },
+
+    {
+        title: "Rise",
+        writer: "legue of legends",
+        img:"banner-bg.jpg",
+    },
+
+    {
+        title: "Rise",
+        writer: "legue of legends",
+        img:"banner-bg.jpg",
+    },
+
+    {
+        title: "Rise",
+        writer: "legue of legends",
+        img:"banner-bg.jpg",
+    },
+
+    {
+        title: "Rise",
+        writer: "legue of legends",
+        img:"banner-bg.jpg",
+    },
+
+    {
+        title: "Rise",
+        writer: "legue of legends",
+        img:"banner-bg.jpg",
+    },
+
+    {
+        title: "Rise",
+        writer: "legue of legends",
+        img:"banner-bg.jpg",
+    },
 ];
 
 const boxes = document.querySelectorAll(".boxes");
-const titles = document.querySelectorAll(".songtitle");
-const writers = document.querySelectorAll(".songwriter");
-
-const items = [];
+const title = document.querySelectorAll(".songtitle");
+const writer = document.querySelectorAll(".songwriter");
 
 window.addEventListener("load",function()
 {
-    for(let i=0;i<SongInformation.length;i++)
+    for(let i=0;i<12;i++)
     {
-        items[i] = SongInformation[i];
+        title[i].textContent =  SongInformation[i].title;
+        writer[i].textContent = SongInformation[i].writer;
     }
-    console.log(items);
-    
 });
+
+
 
 
 // boxes.forEach(function(box) 
@@ -62,45 +97,4 @@ window.addEventListener("load",function()
         
 //     });
 // });
-
-titles.forEach(function(title) 
-{
-    title.addEventListener("click",function(event)
-    {
-        const classitem2 = event.currentTarget.classList;
-        console.log(classitem2);
-        for(let i=0; i<11;i++)
-        {
-            if(classitem2.contains(`title${i}`))
-            {
-                titles.textContent = items[i];
-            }
-        }
-        
-    });
-});
-
-writers.forEach(function(writer) 
-{
-    writer.addEventListener("click",function(event)
-    {
-        const classitem3 = event.currentTarget.classList;
-        console.log(classitem3);
-        
-    });
-});
-
-
-function find()
-{
-    boxes.forEach(function()
-    {
-        let a = boxes.childNodes;
-        console.log(a);
-        
-    });
-}
-
-find();
-
 
